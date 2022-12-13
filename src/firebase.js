d1d505e366868aea8cad6a1e2f828d5b704c07ab
stefancrowley0 <stefancrowley45@gmail.com>
2022-12-13 15:44:31 -0500

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAC7MUfDJ2sIquOFt15frbQximjPz4U81w",
+  authDomain: "fir-crud-b25ef.firebaseapp.com",
+  projectId: "fir-crud-b25ef",
+  storageBucket: "fir-crud-b25ef.appspot.com",
+  messagingSenderId: "56316971189",
+  appId: "1:56316971189:web:341da288781248f77296fc",
+  measurementId: "G-JLLQTQYFK1",
+};
+
+const app = initializeApp(firebaseConfig);
